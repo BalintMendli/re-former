a1b6b63d983d8ada5bb8b16af91bb4a141833fc7
zenott <mblevel@gmail.com>
2019-05-29 22:06:14 +0200

    Add routes and controller for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  get 'users/create'
+  resource :user, only: [:new, :create]
 end
